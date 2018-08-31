264712bff9dbe5af54a4fffbc6c1d4875019697a
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-08-31 14:42:44 -0700

    stats

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 const moment = require("moment");
 const settings = require("../settings.json")
 const colors = require("../data/colors.json")
+const config = require("../config.json")
+const superagent = require('superagent')
 
 module.exports = (client, guild) => { 
     try {
@@ -17,6 +19,12 @@ module.exports = (client, guild) => {
       .setFooter(`Created On - ${moment(guild.createdAt).format('LLLL')}`, guild.iconURL)
       .setTimestamp();
 
+      superagent.post(`https://discordbots.org/api/bots/stats`)
+      .set('Authorization', config.dblToken)
+      .send({ server_count: client.guilds && client.guilds.size ? client.guilds.size : (client.Guilds ? client.Guilds.size : Object.keys(client.Servers).length) })
+      .then(() => console.log('Updated discordbots.org stats'))
+      .catch(err => console.error(`Error updating discordbots.org stats: ${err.body || err}`));
+  
       guild.client.channels.get("483039673057411072").send(guildEmbed);
     } catch (error) {
       console.error(error);
